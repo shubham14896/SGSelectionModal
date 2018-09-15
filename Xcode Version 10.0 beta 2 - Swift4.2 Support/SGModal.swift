b2200acc3eebe251ba1301e3ff0315803bf34d5b
Shubham Gupta <greenshadow@Shubhams-MacBook-Air.local>
2018-09-15 18:57:05 +0530

    Swift 4.2 Support Files

---
+++ Xcode Version 10.0 beta 2 - Swift4.2 Support/SGModal.swift
@@ -0,0 +1,253 @@
+//
+//  SGModal.swift
+//  SGSelectionModal
+//
+//  Created by Shubham Gupta on 10/09/18.
+//  Copyright © 2018 Shubham Gupta. All rights reserved.
+//
+
+import UIKit
+
+open class SGModal: UIView {
+    
+    open var items: [SGModalItem] = []
+    
+    open var titleHeight: CGFloat = 50
+    open var buttonHeight: CGFloat = 50
+    open var cornerRadius: CGFloat = 7
+    open var itemPadding: CGFloat = 10
+    open var minHeight: CGFloat = 200
+    open var width: CGFloat = 150
+    open var selectedIndex: Int = 0
+    
+    open var title: String? = "Title"
+    open var closeButtonTitle: String? = "Close"
+    open var closeButtonColor: UIColor?
+    open var closeButtonColorHighlighted: UIColor?
+    
+    private var scrollY:CGFloat = 0
+    
+    fileprivate var dialogView: UIView?
+    
+    public init() {
+        super.init(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height))
+        setObservers()
+    }
+    
+    public init(title: String, closeButtonTitle cancelString: String) {
+        self.title = title
+        self.closeButtonTitle = cancelString
+        super.init(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height))
+        setObservers()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setObservers()
+    }
+    
+    open func show() {
+        dialogView = createDialogView()
+        guard let dialogView = dialogView else { return }
+        
+        self.layer.shouldRasterize = true
+        self.layer.rasterizationScale = UIScreen.main.scale
+        self.backgroundColor = UIColor(white: 0, alpha: 0)
+        let tapGestures = UITapGestureRecognizer(target: self, action: #selector(SGModal.close))
+        self.addGestureRecognizer(tapGestures)
+        
+        dialogView.layer.opacity = 0.5
+        dialogView.layer.transform = CATransform3DMakeScale(1.3, 1.3, 1)
+        
+        self.addSubview(dialogView)
+        
+        self.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height)
+        UIApplication.shared.keyWindow?.addSubview(self)
+        
+        UIView.animate(withDuration: 0.2, delay: 0, options: UIView.AnimationOptions() , animations: {
+            self.backgroundColor = UIColor(white: 0, alpha: 0.4)
+            dialogView.layer.opacity = 1
+            dialogView.layer.transform = CATransform3DMakeScale(1, 1, 1)
+        }, completion: nil)
+    }
+    
+    @objc open func close() {
+        guard let dialogView = dialogView else { return }
+        let currentTransform = dialogView.layer.transform
+        
+        dialogView.layer.opacity = 1
+        
+        UIView.animate(withDuration: 0.2, delay: 0, options: UIView.AnimationOptions(), animations: {
+            self.backgroundColor = UIColor(white: 0, alpha: 0)
+            dialogView.layer.transform = CATransform3DConcat(currentTransform, CATransform3DMakeScale(0.6, 0.6, 1))
+            dialogView.layer.opacity = 0
+        }, completion: { (finished: Bool) in
+            for view in self.subviews {
+                view.removeFromSuperview()
+            }
+            self.removeFromSuperview()
+        })
+    }
+    
+    open func addItem(item itemTitle: String) {
+        let item = SGModalItem(item: itemTitle)
+        items.append(item)
+    }
+    
+    open func addItem(item itemTitle: String, didTapHandler: @escaping (() -> Void)) {
+        let item = SGModalItem(item: itemTitle, didTapHandler: didTapHandler)
+        items.append(item)
+    }
+    
+    open func addItem(_ item: SGModalItem) {
+        items.append(item)
+    }
+    
+    fileprivate func setObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(SGModal.deviceOrientationDidChange(_:)), name: UIDevice.orientationDidChangeNotification, object: nil)
+    }
+    
+    fileprivate func createDialogView() -> UIView {
+        let screenSize = self.calculateScreenSize()
+        let dialogSize = self.calculateDialogSize()
+        
+        let view = UIView(frame: CGRect(
+            x: (screenSize.width - dialogSize.width) / 2,
+            y: (screenSize.height - dialogSize.height) / 2,
+            width: dialogSize.width,
+            height: dialogSize.height
+        ))
+        
+        view.layer.cornerRadius = cornerRadius
+        view.backgroundColor = UIColor.white
+        view.layer.shadowRadius = cornerRadius
+        view.layer.shadowOpacity = 0.2
+        view.layer.shadowColor = UIColor.black.cgColor
+        
+        view.layer.shouldRasterize = true
+        view.layer.rasterizationScale = UIScreen.main.scale
+        
+        view.addSubview(createTitleLabel())
+        view.addSubview(createContainerView())
+        view.addSubview(createCloseButton())
+        return view
+    }
+    
+    fileprivate func createContainerView() -> UIScrollView {
+        
+        let containerView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: 0))
+        let firstRadioButton = SGRadioButton()
+        let otherButtons:NSMutableArray = NSMutableArray()
+        
+        for (index, item) in items.enumerated() {
+            
+            var radioButton = SGRadioButton()
+            
+            if selectedIndex == index {
+                radioButton = firstRadioButton
+            }
+            
+            radioButton.frame = CGRect(x: 0, y: CGFloat(index*50), width: width-10, height: 49)
+            radioButton.setTitle(item.itemTitle, for: UIControl.State())
+            radioButton.titleLabel?.font = UIFont.systemFont(ofSize: 14.0)
+            radioButton.setTitleColor(UIColor.black, for: UIControl.State())
+            radioButton.iconOnRight = true;
+            radioButton.multipleSelectionEnabled = false
+            radioButton.iconColor = UIColor.black
+            radioButton.indicatorColor = UIColor.black
+            radioButton.addTarget(item, action: #selector(SGModalItem.handlerTap), for: .touchUpInside)
+            containerView.addSubview(radioButton)
+            otherButtons.add(radioButton)
+            
+            if selectedIndex == index {
+                radioButton.isSelected = true
+                scrollY = CGFloat(index * 50)
+            }
+            
+            let divider = UIView(frame: CGRect(x: 0, y: CGFloat(index*50)+50, width: width, height: 0.5))
+            divider.backgroundColor = UIColor.lightGray
+            containerView.addSubview(divider)
+            containerView.frame.size.height += 50
+        }
+        
+        firstRadioButton.otherButtons = (otherButtons as AnyObject as! Array<SGRadioButton>)
+        
+        let scrollView = UIScrollView(frame: CGRect(x: 0, y: titleHeight, width: width, height: minHeight))
+        scrollView.contentSize.height = CGFloat(items.count*50)
+        scrollView.addSubview(containerView)
+        scrollView.flashScrollIndicators()
+        
+        let maxScrollY = scrollView.contentSize.height - scrollView.bounds.size.height
+        let bottomOffset = CGPoint(x: 0, y: scrollY > maxScrollY ? maxScrollY : scrollY)
+        scrollView.setContentOffset(bottomOffset, animated: true)
+        
+        return scrollView
+    }
+    
+    fileprivate func createTitleLabel() -> UIView {
+        
+        let view = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: titleHeight))
+        view.text = title
+        view.textAlignment = .center
+        view.font = UIFont.boldSystemFont(ofSize: 18.0)
+        
+        let bottomLayer = CALayer()
+        bottomLayer.frame = CGRect(x: 0, y: view.bounds.size.height, width: view.bounds.size.width, height: 0.5)
+        bottomLayer.backgroundColor = UIColor(red: 198/255, green: 198/255, blue: 198/255, alpha: 1).cgColor
+        view.layer.addSublayer(bottomLayer)
+        
+        return view
+    }
+    
+    fileprivate func createCloseButton() -> UIButton {
+        let minValue = min(CGFloat(items.count)*50.0, minHeight)
+        let button = UIButton(frame: CGRect(x: 0, y: titleHeight + minValue, width: width, height: buttonHeight))
+        
+        button.addTarget(self, action: #selector(SGModal.close), for: UIControl.Event.touchUpInside )
+        
+        let colorNormal = closeButtonColor != nil ? closeButtonColor : button.tintColor
+        let colorHighlighted = closeButtonColorHighlighted != nil ? closeButtonColorHighlighted : colorNormal?.withAlphaComponent(0.5)
+        
+        button.setTitle(closeButtonTitle, for: UIControl.State())
+        button.setTitleColor(colorNormal, for: UIControl.State())
+        button.setTitleColor(colorHighlighted, for: UIControl.State.highlighted)
+        button.setTitleColor(colorHighlighted, for: UIControl.State.disabled)
+        
+        let topLayer = CALayer()
+        topLayer.frame = CGRect(x: 0, y: 0, width: 300, height: 0.5)
+        topLayer.backgroundColor = UIColor(red: 198/255, green: 198/255, blue: 198/255, alpha: 1).cgColor
+        button.layer.addSublayer(topLayer)
+        
+        return button
+    }
+    
+    fileprivate func calculateDialogSize() -> CGSize {
+        let minValue = min(CGFloat(items.count)*50.0, minHeight)
+        return CGSize(width: width, height: minValue + titleHeight + buttonHeight)
+    }
+    
+    fileprivate func calculateScreenSize() -> CGSize {
+        let width = UIScreen.main.bounds.width
+        let height = UIScreen.main.bounds.height
+        return CGSize(width: width, height: height)
+    }
+    
+    @objc internal func deviceOrientationDidChange(_ notification: Notification) {
+        self.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+        
+        let screenSize = self.calculateScreenSize()
+        let dialogSize = self.calculateDialogSize()
+        
+        dialogView?.frame = CGRect(
+            x: (screenSize.width - dialogSize.width) / 2,
+            y: (screenSize.height - dialogSize.height) / 2,
+            width: dialogSize.width,
+            height: dialogSize.height
+        )
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIDevice.orientationDidChangeNotification, object: nil)
+    }
+}
+
---
+++ Xcode Version 10.0 beta 2 - Swift4.2 Support/SGRadioButton.swift
@@ -0,0 +1,348 @@
+//
+//  SGRadioButton.swift
+//  SGSelectionModal
+//
+//  Created by Shubham Gupta on 10/09/18.
+//  Copyright © 2018 Shubham Gupta. All rights reserved.
+//
+
+import UIKit
+
+public class SGRadioButton: UIButton {
+    
+    var indexPath:IndexPath!
+    
+    public var animationDuration:CFTimeInterval = 0.03
+    
+    static let kGeneratedIconName:String = "Rendered Icon"
+    
+    static var  _groupModifing:Bool = false
+    
+    var otherButtons: Array<SGRadioButton>?
+    
+    public var iconSize:CGFloat = 15.0
+    
+    public var indicatorSize:CGFloat = 15.0 * 0.5
+    
+    public var iconColor:UIColor =  UIColor.black
+    
+    public var iconStrokeWidth:CGFloat = 1.6
+    
+    public var indicatorColor:UIColor = UIColor.black
+    
+    public var marginWidth:CGFloat = 10.0
+    
+    public var iconOnRight:Bool = false
+    
+    public var iconSquare:Bool = false
+    
+    public var icon:UIImage!
+    
+    public var iconSelected:UIImage!
+    
+    public var multipleSelectionEnabled:Bool = false
+    
+    required public init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        self.initRadioButton()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.initRadioButton()
+    }
+    
+    override public func draw(_ rect:CGRect) {
+        super.draw(rect)
+        self.drawButton()
+    }
+    
+    private var setOtherButtons:NSArray {
+        get{
+            return otherButtons! as NSArray
+        }
+        set (newValue) {
+            if (!self.isGroupModifing()){
+                self.groupModifing(chaining: true)
+                let otherNewButtons:Array<SGRadioButton>? = newValue as? Array<SGRadioButton>
+                for radioButton in otherNewButtons!{
+                    let otherButtonsForCurrentButton:NSMutableArray = NSMutableArray(array:otherNewButtons!)
+                    otherButtonsForCurrentButton.add(self)
+                    otherButtonsForCurrentButton.remove(radioButton)
+                    radioButton.setOtherButtons = otherButtonsForCurrentButton
+                }
+                self.groupModifing(chaining: false)
+            }
+            self.otherButtons = newValue as? Array<SGRadioButton>
+        }
+    }
+    
+    public var setIcon:UIImage {
+        
+        get{
+            return icon
+        }
+        
+        set (newValue){
+            icon = newValue
+            self.setImage(icon, for: .normal)
+        }
+    }
+    
+    public var setIconSelected:UIImage {
+        
+        get{
+            return iconSelected
+        }
+        
+        set (newValue){
+            iconSelected = newValue
+            self.setImage(iconSelected, for: .selected)
+            self.setImage(iconSelected, for: .highlighted)
+        }
+    }
+    
+    public var setAnimationDuration:CFTimeInterval{
+        get {
+            return animationDuration
+        }
+        
+        set(newValue) {
+            if (!self.isGroupModifing()){
+                self.groupModifing(chaining: true)
+                if self.otherButtons != nil {
+                    for radioButton in self.otherButtons!{
+                        radioButton.animationDuration = newValue
+                    }
+                }
+                self.groupModifing(chaining: false)
+            }
+            animationDuration = newValue
+        }
+    }
+    
+    public var setMultipleSelectionEnabled:Bool {
+        
+        get{
+            return multipleSelectionEnabled
+        }
+        set (newValue) {
+            if (!self.isGroupModifing()){
+                self.groupModifing(chaining: true)
+                if self.otherButtons != nil {
+                    for radioButton in self.otherButtons!{
+                        radioButton.multipleSelectionEnabled = newValue
+                    }
+                }
+                self.groupModifing(chaining: false)
+            }
+            multipleSelectionEnabled = newValue
+        }
+    }
+    
+    func drawButton (){
+        if (self.icon == nil){
+            self.setIcon = self.drawIconWithSelection(false)
+        }else{
+            self.setIcon = self.icon ;
+        }
+        
+        if (iconSelected == nil){
+            self.setIconSelected = self.drawIconWithSelection(true)
+        }else{
+            self.setIconSelected = self.iconSelected;
+        }
+        
+        if self.otherButtons != nil {
+            self.setOtherButtons = self.otherButtons! as NSArray
+        }
+        
+        if multipleSelectionEnabled {
+            self.setMultipleSelectionEnabled = multipleSelectionEnabled
+        }
+        
+        if self.iconOnRight {
+            self.imageEdgeInsets = UIEdgeInsets(top: 0, left: self.frame.size.width - self.icon.size.width + marginWidth - 20, bottom: 0, right: 0);
+            self.titleEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: marginWidth + self.icon.size.width);
+            self.contentHorizontalAlignment = .left
+            self.titleLabel?.textAlignment = .left
+        } else {
+            self.titleEdgeInsets = UIEdgeInsets(top: 0, left: marginWidth, bottom: 0, right: 0);
+            self.titleLabel?.textAlignment = .left
+            self.contentHorizontalAlignment = .left
+        }
+        self.titleLabel?.adjustsFontSizeToFitWidth = false
+    }
+    
+    func drawIconWithSelection (_ selected:Bool) -> UIImage{
+        
+        let rect:CGRect = CGRect(x: 0, y: 0, width: iconSize, height: iconSize)
+        let context  = UIGraphicsGetCurrentContext()
+        UIGraphicsPushContext(context!)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0);
+        
+        var iconPath:UIBezierPath!
+        let iconRect:CGRect = CGRect(x: iconStrokeWidth / 2, y: iconStrokeWidth / 2, width: iconSize - iconStrokeWidth, height: iconSize - iconStrokeWidth);
+        if self.iconSquare {
+            iconPath = UIBezierPath(rect:iconRect )
+        } else {
+            iconPath = UIBezierPath(ovalIn:iconRect)
+        }
+        iconColor.setStroke()
+        iconPath.lineWidth = iconStrokeWidth;
+        iconPath.stroke()
+        context?.addPath(iconPath.cgPath);
+        
+        if (selected) {
+            var indicatorPath:UIBezierPath!
+            let indicatorRect:CGRect = CGRect(x: (iconSize - indicatorSize) / 2, y: (iconSize - indicatorSize) / 2, width: indicatorSize, height: indicatorSize);
+            if self.iconSquare {
+                indicatorPath = UIBezierPath(rect:indicatorRect )
+            } else {
+                indicatorPath = UIBezierPath(ovalIn:indicatorRect)
+            }
+            indicatorColor.setStroke()
+            indicatorPath.lineWidth = iconStrokeWidth;
+            indicatorPath.stroke()
+            
+            indicatorColor.setFill()
+            indicatorPath.fill()
+            context?.addPath(indicatorPath.cgPath);
+        }
+        
+        let image:UIImage = UIGraphicsGetImageFromCurrentImageContext()!;
+        UIGraphicsPopContext()
+        UIGraphicsEndImageContext();
+        image.accessibilityIdentifier = SGRadioButton.kGeneratedIconName;
+        return image;
+    }
+    
+    @objc func touchDown () {
+        //if self.isSelected {
+        //self.isSelected = false
+        //}else{
+        self.isSelected = true
+        //}
+    }
+    
+    func initRadioButton () {
+        super.addTarget(self, action:#selector(SGRadioButton.touchDown), for:.touchUpInside)
+        //self.isSelected = false
+    }
+    
+    override public func prepareForInterfaceBuilder () {
+        self.initRadioButton()
+        self.drawButton()
+    }
+    
+    
+    func groupModifing(chaining:Bool) {
+        SGRadioButton._groupModifing = chaining
+    }
+    
+    func isGroupModifing() -> Bool {
+        return SGRadioButton._groupModifing
+    }
+    
+    public func selectedButton() -> SGRadioButton!{
+        if !self.multipleSelectionEnabled {
+            if self.isSelected {
+                return self
+            }
+        }else{
+            for isRadioButton in self.otherButtons!  {
+                if isRadioButton.isSelected {
+                    return isRadioButton
+                }
+            }
+        }
+        return nil
+    }
+    
+    public func selectedButtons() -> NSMutableArray{
+        
+        let selectedButtons:NSMutableArray = NSMutableArray ()
+        if self.isSelected {
+            selectedButtons.add(self)
+        }
+        for radioButton in self.otherButtons!  {
+            if radioButton.isSelected {
+                selectedButtons .add(self)
+            }
+        }
+        return selectedButtons;
+    }
+    
+    public func deselectOtherButtons() {
+        if self.otherButtons != nil {
+            for radioButton in self.otherButtons!  {
+                radioButton.isSelected = false
+            }
+        }
+    }
+    
+    public func unSelectedButtons() -> NSArray{
+        let unSelectedButtons:NSMutableArray = NSMutableArray ()
+        if self.isSelected == false {
+            unSelectedButtons.add(self)
+        }
+        for isRadioButton in self.otherButtons!  {
+            if isRadioButton.isSelected == false {
+                unSelectedButtons.add(self)
+            }
+        }
+        return unSelectedButtons ;
+    }
+    
+    override public func titleColor(for state:UIControl.State) -> UIColor{
+        if (state == UIControl.State.selected || state == UIControl.State.highlighted){
+            var selectedOrHighlightedColor:UIColor!
+            if (state == UIControl.State.selected) {
+                selectedOrHighlightedColor = super.titleColor(for: .selected)
+            }else{
+                selectedOrHighlightedColor = super.titleColor(for: .highlighted)
+            }
+            self.setTitleColor(selectedOrHighlightedColor, for: .selected)
+            self.setTitleColor(selectedOrHighlightedColor, for: .highlighted)
+        }
+        return super.titleColor(for: state)!
+    }
+    
+    override public var isSelected: Bool {
+        
+        didSet {
+            
+            if (multipleSelectionEnabled || oldValue != self.isSelected && self.animationDuration > 0.0) {
+                
+                if self.iconSelected != nil && self.icon != nil {
+                    let animation = CABasicAnimation(keyPath: "contents")
+                    if self.isSelected {
+                        animation.fromValue = self.iconSelected.cgImage
+                    }else{
+                        animation.fromValue = self.icon.cgImage
+                    }
+                    
+                    if self.isSelected {
+                        animation.toValue = self.icon.cgImage
+                    }else{
+                        animation.toValue = self.iconSelected.cgImage
+                    }
+                    animation.duration = self.animationDuration
+                    self.imageView?.layer.add(animation, forKey:"icon" )
+                }
+            }
+            
+            if (multipleSelectionEnabled) {
+                if oldValue == true && self.isSelected == true {
+                    super.isSelected = false
+                }else{
+                    super.isSelected = true
+                }
+            }else {
+                if ( oldValue == false && self.isSelected == true ) {
+                    self.deselectOtherButtons()
+                }
+            }
+        }
+    }
+}
